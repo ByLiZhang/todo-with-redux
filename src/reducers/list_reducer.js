ecadf50fd6143c314e9cf6993c80b61601026b9e
Li Zhang <bylizhang@gmail.com>
2018-04-06 15:36:25 -0700

    Completed getting list data from server

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -1,11 +1,15 @@
 import types from '../actions/types';
 
+
 const DEFAULT_STATE = {
     items: []
 };
 
 export default function(state = DEFAULT_STATE, action){
     switch(action.type){
+    	case types.GET_LIST_DATA:
+    		console.log('LIST REDUCER - GET LIST:', action.payload);
+    		return {...state, items: action.payload.data.todos};
         default:
             return state;
     }
